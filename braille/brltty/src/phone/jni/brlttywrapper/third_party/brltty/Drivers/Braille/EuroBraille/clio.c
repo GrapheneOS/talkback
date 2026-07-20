3f21be743bc9bce13ad6eaf439b9acc99889ccd6
Artem Smirnov <artem@smirnov.page>
2026-07-20 22:52:27 +0300

    brltty: drop dead files and finish the EuroBraille rename
    Follow-up tidy-up to the 6.9.1 update; none of these files are compiled.
    
    - Removed files that upstream deleted: the Virtual braille driver, the
      Headers/strfmth.h header (superseded upstream by strfmt_types.h) and
      Programs/brltty-lscmds.c.
    - EuroBraille (a disabled driver kept for its "gets back" TODO) was left
      half-renamed by the bulk re-vendor; completed the 6.9.1 rename of
      eu_braille.c/eu_clio.c/eu_esysiris.c to braille.c/clio.c/esysiris.c so
      the directory matches upstream 6.9.1.
    
    Native library still builds and links for both ABIs.
    
    Signed-off-by: Artem Smirnov <artem@smirnov.page>

--- braille/brltty/src/phone/jni/brlttywrapper/third_party/brltty/Drivers/Braille/EuroBraille/clio.c
+++ braille/brltty/src/phone/jni/brlttywrapper/third_party/brltty/Drivers/Braille/EuroBraille/clio.c
@@ -2,7 +2,7 @@
  * BRLTTY - A background process providing access to the console screen (when in
  *          text mode) for a blind person using a refreshable braille display.
  *
- * Copyright (C) 1995-2024 by The BRLTTY Developers.
+ * Copyright (C) 1995-2026 by The BRLTTY Developers.
  *
  * BRLTTY comes with ABSOLUTELY NO WARRANTY.
  *
@@ -111,14 +111,15 @@ typedef struct {
   char modelCode[3];
   const char *modelName;
   unsigned char cellCount;
-  unsigned isAzerBraille:1;
-  unsigned isEuroBraille:1;
-  unsigned isIris:1;
-  unsigned isNoteBraille:1;
-  unsigned isPupiBraille:1;
-  unsigned isScriba:1;
-  unsigned hasRoutingKeys:1;
-  unsigned hasVisualDisplay:1;
+
+  unsigned char isAzerBraille:1;
+  unsigned char isEuroBraille:1;
+  unsigned char isIris:1;
+  unsigned char isNoteBraille:1;
+  unsigned char isPupiBraille:1;
+  unsigned char isScriba:1;
+  unsigned char hasRoutingKeys:1;
+  unsigned char hasVisualDisplay:1;
 } ModelEntry;
 
 static const ModelEntry modelTable[] = {
